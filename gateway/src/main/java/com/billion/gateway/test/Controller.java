0ff898bad24853bef68aed22bb06ede0f75bf6cb
leeqiang250 <leeqiang250@163.com>
2022-08-14 01:15:14 +0800

    no message

---
+++ gateway/src/main/java/com/billion/gateway/test/Controller.java
@@ -0,0 +1,25 @@
+package com.billion.gateway.test;
+
+import com.billion.model.response.Response;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liqiang
+ */
+@RestController
+@RequestMapping(path = {"/","/test"})
+public class Controller {
+
+    @GetMapping(path = {"/", "/success"})
+    public Response success() {
+        return Response.success();
+    }
+
+    @GetMapping("/failure")
+    public Response failure() {
+        return Response.failure();
+    }
+
+}
