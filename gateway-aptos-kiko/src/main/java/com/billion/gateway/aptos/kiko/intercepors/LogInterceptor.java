4ef779c34fc3ee3bba42013834ae1280320708da
任健 <renjian@192.168.0.100>
2022-09-15 18:31:23 +0800

    新增拦截器，优化接口日志

---
+++ gateway-aptos-kiko/src/main/java/com/billion/gateway/aptos/kiko/intercepors/LogInterceptor.java
@@ -0,0 +1,57 @@
+package com.billion.gateway.aptos.kiko.intercepors;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+@Slf4j
+@Component
+public class LogInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        initThreadName(request);
+        return true;
+    }
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+    }
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        log.info("[THREAD_END][{}]", (request).getRequestURI());
+    }
+
+    private void initThreadName(HttpServletRequest request) {
+        String ip = getIpAddr(request);
+        int random = (int)(Math.random() * 100);
+        String threadName = new SimpleDateFormat("HHmmssSSS").format(new Timestamp(System.currentTimeMillis())) + String.valueOf(random) ;
+        Thread.currentThread().setName(threadName);
+        log.info("[THREAD_BEGIN][{}][{}]", ip, (request).getRequestURI());
+    }
+
+    private static String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
+}
---
+++ gateway-aptos-kiko/src/main/java/com/billion/gateway/aptos/kiko/intercepors/WebConfigurer.java
@@ -0,0 +1,27 @@
+package com.billion.gateway.aptos.kiko.intercepors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfigurer implements WebMvcConfigurer{
+
+
+    @Autowired
+    LogInterceptor logInterceptor;
+
+    // 配置静态资源的，比如html，js，css，等等
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    }
+
+    // 注册拦截器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor).addPathPatterns("/**");
+    }
+
+}
