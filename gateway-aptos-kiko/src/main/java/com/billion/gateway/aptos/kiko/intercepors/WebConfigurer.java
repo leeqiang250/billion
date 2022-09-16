f253beac6aabccfdd86745dd14c596bd2184b841
leeqiang250 <leeqiang250@163.com>
2022-09-16 12:31:57 +0800

    no message

--- gateway-aptos-kiko/src/main/java/com/billion/gateway/aptos/kiko/intercepors/WebConfigurer.java
+++ gateway-aptos-kiko/src/main/java/com/billion/gateway/aptos/kiko/intercepors/WebConfigurer.java
@@ -1,26 +1,20 @@
 package com.billion.gateway.aptos.kiko.intercepors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.Resource;
 
+/**
+ * @author liqiang
+ */
 @Configuration
 public class WebConfigurer implements WebMvcConfigurer {
-
 
     @Resource
     LogInterceptor logInterceptor;
 
-    // 配置静态资源的，比如html，js，css，等等
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    }
-
-    // 注册拦截器
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(logInterceptor).addPathPatterns("/**");
