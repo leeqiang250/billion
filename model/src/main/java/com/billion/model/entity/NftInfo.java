5f66b9e8efa47fded4faef01f0bc0c673177e3ef
leeqiang250 <leeqiang250@163.com>
2022-09-18 19:31:28 +0800

    no message

--- model/src/main/java/com/billion/model/entity/NftInfo.java
+++ model/src/main/java/com/billion/model/entity/NftInfo.java
@@ -10,8 +10,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
-
 /**
  * @author liqiang
  */
@@ -39,77 +37,5 @@ public class NftInfo implements IModel {
      */
     @TableField("group_id")
     Long groupId;
-
-    /**
-     * 类型: nft:不可拆解的nft,composit_element:组成卡牌的元素,composite_card:卡牌
-     */
-    @TableField("type")
-    String type;
-
-    /**
-     * 名称
-     */
-    @TableField("name")
-    String name;
-
-    /**
-     * 所有者地址
-     */
-    @TableField("owner")
-    String owner;
-
-    /**
-     * 图片链接
-     */
-    @TableField("image_link")
-    String imageLink;
-
-    /**
-     * 图片内容
-     */
-    @TableField("image_data")
-    String imageData;
-
-    /**
-     * 分数
-     */
-    @TableField("score")
-    BigDecimal score;
-
-    /**
-     * 排名
-     */
-    @TableField("rank")
-    Integer rank;
-
-    /**
-     * 已创建
-     */
-    @TableField("created")
-    Boolean created;
-
-    /**
-     * 状态: init,success,invalid
-     */
-    @TableField("state")
-    String state;
-
-    /**
-     * 开盲盒时间(单位:毫秒)
-     */
-    @TableField("open_box_time")
-    Long openBoxTime;
-
-    /**
-     * 创建时间
-     */
-    @TableField("create_time")
-    Long createTime;
-
-    /**
-     * 更新时间
-     */
-    @TableField("update_time")
-    Long updateTime;
 
 }
