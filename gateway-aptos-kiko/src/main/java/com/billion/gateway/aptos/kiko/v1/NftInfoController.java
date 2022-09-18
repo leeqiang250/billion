1961ddfc94d97831baaf186ed9c359d1bae2a5e5
leeqiang250 <leeqiang250@163.com>
2022-09-18 23:43:33 +0800

    no message

--- gateway-aptos-kiko/src/main/java/com/billion/gateway/aptos/kiko/v1/NftInfoController.java
+++ gateway-aptos-kiko/src/main/java/com/billion/gateway/aptos/kiko/v1/NftInfoController.java
@@ -1,7 +1,11 @@
 package com.billion.gateway.aptos.kiko.v1;
 
 import com.billion.model.controller.IController;
+import com.billion.model.dto.Context;
 import com.billion.model.entity.NftInfo;
+import com.billion.model.enums.Authenticate;
+import com.billion.model.enums.AuthenticateType;
+import com.billion.model.response.Response;
 import com.billion.model.service.ICacheService;
 import com.billion.service.aptos.kiko.NftInfoService;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +29,18 @@ public class NftInfoController implements IController<NftInfo> {
     @Override
     public ICacheService<NftInfo> service() {
         return this.nftInfoService;
+    }
+
+    @Override
+    @Authenticate(AuthenticateType.FORBID)
+    public Response cacheList(Context context) {
+        return IController.super.cacheList(context);
+    }
+
+    @Override
+    @Authenticate(AuthenticateType.FORBID)
+    public Response cacheMap(Context context) {
+        return IController.super.cacheMap(context);
     }
 
 }
