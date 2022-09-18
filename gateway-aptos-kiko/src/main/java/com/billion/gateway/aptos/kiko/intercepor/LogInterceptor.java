8bae6b0f83af178b95f079afd890912064aeeb53
leeqiang250 <leeqiang250@163.com>
2022-09-18 23:37:33 +0800

    no message

--- gateway-aptos-kiko/src/main/java/com/billion/gateway/aptos/kiko/intercepor/LogInterceptor.java
+++ gateway-aptos-kiko/src/main/java/com/billion/gateway/aptos/kiko/intercepor/LogInterceptor.java
@@ -19,12 +19,14 @@ public class LogInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         log.info("------------------------------------------------------------------------------------------------");
         log.info("[request begin] uri:[{}]", request.getRequestURI());
+        request.setAttribute("k-ts", System.currentTimeMillis());
         return true;
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
-        log.info("[request end] uri:[{}]", request.getRequestURI());
+        log.info("[request end] uri:[{}] elapsed:[{} ms]", request.getRequestURI(), System.currentTimeMillis() - Long.parseLong(request.getAttribute("k-ts").toString()));
+        log.info("------------------------------------------------------------------------------------------------");
     }
 
 }
