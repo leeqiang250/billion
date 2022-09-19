5e5cf777b3ff7c3f6384b84d5f2fb0d8728d1ec3
leeqiang250 <leeqiang250@163.com>
2022-09-19 10:12:47 +0800

    no message

---
+++ model/src/main/java/com/billion/model/constant/v1/RequestPathAccountV1.java
@@ -0,0 +1,12 @@
+package com.billion.model.constant.v1;
+
+import static com.billion.model.constant.RequestPath.V1;
+
+/**
+ * @author liqiang
+ */
+public interface RequestPathAccountV1 {
+
+    String ACCOUNT = V1 + "/account";
+
+}
---
+++ model/src/main/java/com/billion/model/constant/v1/RequestPathMarketV1.java
@@ -0,0 +1,12 @@
+package com.billion.model.constant.v1;
+
+import static com.billion.model.constant.RequestPath.V1;
+
+/**
+ * @author liqiang
+ */
+public interface RequestPathMarketV1 {
+
+    String MARKET = V1 + "/market";
+
+}
