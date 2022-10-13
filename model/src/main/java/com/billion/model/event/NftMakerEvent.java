dfcbb661197754b8f4754429eed3d0f7d09f3029
leeqiang250 <leeqiang250@163.com>
2022-10-13 15:45:27 +0800

    no message

---
+++ model/src/main/java/com/billion/model/event/NftMakerEvent.java
@@ -0,0 +1,48 @@
+package com.billion.model.event;
+
+import com.alibaba.fastjson2.annotation.JSONField;
+import com.aptos.request.v1.model.TokenId;
+import com.aptos.utils.Hex;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author liqiang
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NftMakerEvent implements Serializable {
+
+    String id;
+
+    String type;
+
+    String maker;
+
+    String price;
+
+    @JSONField(name = "token_id")
+    TokenId tokenId;
+
+    String amount;
+
+    String ts;
+
+    @JSONField(name = "dead_ts")
+    String deadTs;
+
+    public void setType(String type) {
+        if (type.startsWith("0x")) {
+            this.type = Hex.decodeToString(type);
+        } else {
+            this.type = type;
+        }
+    }
+
+}
---
+++ model/src/main/java/com/billion/model/event/NftTakerEvent.java
@@ -0,0 +1,59 @@
+package com.billion.model.event;
+
+import com.alibaba.fastjson2.annotation.JSONField;
+import com.aptos.request.v1.model.TokenId;
+import com.aptos.utils.Hex;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author liqiang
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NftTakerEvent implements Serializable {
+
+    String id;
+
+    String type;
+
+    String maker;
+
+    String price;
+
+    @JSONField(name = "token_id")
+    TokenId tokenId;
+
+    String amount;
+
+    String ts;
+
+    @JSONField(name = "dead_ts")
+    String deadTs;
+
+    String bidder;
+
+    @JSONField(name = "final_price")
+    String finalPrice;
+
+    @JSONField(name = "creator_fee")
+    String creatorFee;
+
+    @JSONField(name = "platform_fee")
+    String platformFee;
+
+    public void setType(String type) {
+        if (type.startsWith("0x")) {
+            this.type = Hex.decodeToString(type);
+        } else {
+            this.type = type;
+        }
+    }
+
+}
