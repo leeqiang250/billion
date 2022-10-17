d9e307a042c374190918c4a5fa568eeb216a9f3c
leeqiang250 <leeqiang250@163.com>
2022-10-17 19:01:33 +0800

    no message

--- shell-gateway-aptos-kiko.sh
+++ shell-gateway-aptos-kiko.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 
-app_name='gateway-aptos-kiko'
-enviroment='test'
-app_mem='512m'
+app_name=gateway-aptos-kiko
+enviroment=test
+app_mem=512m
 
-base_home=/home/ubuntu/$app_name
-errorlogs_dir=$base_home/logs
-gclog_file=$base_home/logs/${app_name}-gc.log
-dump_dir=$base_home/heapdump
+base_home=/home/ubuntu/${app_name}
+errorlogs_dir=${base_home}/logs
+gclog_file=${base_home}/logs/${app_name}-gc.log
+dump_dir=${base_home}/heapdump
 
 java -Xmx${app_mem} -Xms${app_mem} -XX:+UseParallelGC -XX:+UseParallelOldGC -XX:+PrintGCDetails -Xloggc:${gclog_file} -XX:HeapDumpPath=${dump_dir} -XX:+HeapDumpOnOutOfMemoryError  -XX:ErrorFile=${errorlogs_dir}/hs_err_%p.log  -Dfile.encoding=UTF-8 -Duser.timezone="Asia/Singapore" -Dspring.profiles.active=${enviroment} -jar ${base_home}/target/${app_name}.jar >${base_home}/logs/${app_name}-info.log 2>&1 &
