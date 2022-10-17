b318c99cb284301b2bbdf9bb154a5834fbb16b82
leeqiang250 <leeqiang250@163.com>
2022-10-17 20:53:58 +0800

    no message

--- shell-gateway-aptos-kiko.sh
+++ shell-gateway-aptos-kiko.sh
@@ -9,6 +9,10 @@ errorlogs_dir=${base_home}/logs
 gclog_file=${base_home}/logs/${app_name}-gc.log
 dump_dir=${base_home}/heapdump
 
-java -Dspring.profiles.active=test -jar ./${app_name}.jar &
+kill -9 `ps -ef | grep ${app_name} | grep -v grep | awk '{print $2}'`
+
+sleep 10
+
+java -Dspring.profiles.active=${enviroment} -jar ./${app_name}.jar &
 
 #java -Xmx${app_mem} -Xms${app_mem} -XX:+UseParallelGC -XX:+UseParallelOldGC -XX:+PrintGCDetails -Xloggc:${gclog_file} -XX:HeapDumpPath=${dump_dir} -XX:+HeapDumpOnOutOfMemoryError  -XX:ErrorFile=${errorlogs_dir}/hs_err_%p.log  -Dfile.encoding=UTF-8 -Duser.timezone="Asia/Singapore" -jar ${base_home}/target/${app_name}.jar >${base_home}/logs/${app_name}-info.log 2>&1 spring.profiles.active=${enviroment} &
