c0abaea813124af88b39552bdd4e0a2cc35ae61c
leeqiang250 <leeqiang250@163.com>
2022-10-17 20:47:52 +0800

    no message

--- gateway-aptos-kiko/src/main/java/com/billion/gateway/aptos/kiko/GatewayApplication.java
+++ gateway-aptos-kiko/src/main/java/com/billion/gateway/aptos/kiko/GatewayApplication.java
@@ -9,18 +9,12 @@ import org.springframework.context.annotation.ComponentScan;
 /**
  * @author liqiang
  */
-@Slf4j
 @SpringBootApplication
 @MapperScan({"com.billion.dao"})
 @ComponentScan({"com.billion.service", "com.billion.model", "com.billion.gateway"})
 public class GatewayApplication {
 
     public static void main(String[] args) {
-        log.info("------------------------------------------------------------------------------------------------");
-        for (String arg : args) {
-            log.info(arg);
-        }
-        log.info("------------------------------------------------------------------------------------------------");
         SpringApplication.run(GatewayApplication.class, args);
     }
 
