e60a10b7169f9cb3983fd72232e6c63d34360651
leeqiang250 <leeqiang250@163.com>
2022-10-17 22:43:12 +0800

    no message

--- shell/gateway-aptos-kiko/build-gateway-aptos-kiko.sh
+++ shell/gateway-aptos-kiko/build-gateway-aptos-kiko.sh
@@ -11,14 +11,14 @@ mvn clean -DskipTests=true install
 app_name=gateway-aptos-kiko
 
 cd /home/ubuntu/source-code
-git clone https://github.com/leeqiang250/billion.git ${app_name}
-cd ${app_name}
+git clone https://github.com/leeqiang250/billion.git
+cd billion
 mvn clean -DskipTests=true package
 
 mkdir -p /home/ubuntu/${app_name}/logs
 
 rm -rf /home/ubuntu/${app_name}/${app_name}.jar
 cp /home/ubuntu/source-code/billion/${app_name}/target/${app_name}-0.0.1-SNAPSHOT.jar /home/ubuntu/${app_name}/${app_name}.jar
-cp /home/ubuntu/source-code/billion/shell/${app_name}/${app_name}.sh /home/ubuntu/${app_name}/restart.sh
+cp /home/ubuntu/source-code/billion/shell/${app_name}/restart.sh /home/ubuntu/${app_name}/restart.sh
 
 sh /home/ubuntu/${app_name}/restart.sh
