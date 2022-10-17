c87890e6178e55e2c8a6ab97d1c3f58d4b8a5a95
leeqiang250 <leeqiang250@163.com>
2022-10-17 19:04:29 +0800

    no message

--- shell-build.sh
+++ shell-build.sh
@@ -17,9 +17,8 @@ gateway_aptos_kiko=gateway-aptos-kiko
 
 mkdir -p /home/ubuntu/${gateway_aptos_kiko}/config
 mkdir -p /home/ubuntu/${gateway_aptos_kiko}/logs
-mkdir -p /home/ubuntu/${gateway_aptos_kiko}/shell
 mkdir -p /home/ubuntu/${gateway_aptos_kiko}/target
 
 rm -rf /home/ubuntu/${gateway_aptos_kiko}/target/${gateway_aptos_kiko}.jar
 cp /home/ubuntu/source-code/billion/${gateway_aptos_kiko}/target/${gateway_aptos_kiko}-0.0.1-SNAPSHOT.jar /home/ubuntu/${gateway_aptos_kiko}/target/${gateway_aptos_kiko}.jar
-cp /home/ubuntu/source-code/billion/shell-${gateway_aptos_kiko}.sh /home/ubuntu/${gateway_aptos_kiko}/shell/restart.sh
+cp /home/ubuntu/source-code/billion/shell-${gateway_aptos_kiko}.sh /home/ubuntu/${gateway_aptos_kiko}/restart.sh
