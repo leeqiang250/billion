b9f3fe21c3d22b8e8bcc818ba05aaeada5ed5550
leeqiang250 <leeqiang250@163.com>
2022-10-20 18:01:39 +0800

    no message

---
+++ model/src/main/java/com/billion/model/dto/NftAttribute.java
@@ -0,0 +1,34 @@
+package com.billion.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 铸造时：通过NFT ID查询nft_attribute_value的全部数据
+ *
+ * @author liqiang
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NftAttribute {
+
+    /**
+     * nft_attribute_type::class_name
+     */
+    String type;
+
+    /**
+     * nft_attribute_meta::attribute
+     */
+    String key;
+
+    /**
+     * nft_attribute_meta::value
+     */
+    String value;
+
+}
