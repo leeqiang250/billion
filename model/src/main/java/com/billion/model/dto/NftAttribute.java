fb93e2b4f14ff2615e6d69a43b47bcf83db02839
leeqiang250 <leeqiang250@163.com>
2022-10-20 18:48:27 +0800

    no message

--- model/src/main/java/com/billion/model/dto/NftAttribute.java
+++ model/src/main/java/com/billion/model/dto/NftAttribute.java
@@ -18,16 +18,23 @@ public class NftAttribute {
 
     /**
      * nft_attribute_type::class_name
+     * 衣服：nft_attribute_clothes_{nft_attribute_type_id}
+     * 性别：nft_attribute_sex_{nft_attribute_type_id}
      */
     String type;
 
     /**
      * nft_attribute_meta::attribute
+     * 衣服/蓝色：nft_attribute_clothes_blue_{nft_attribute_meta_id}
+     * 衣服/红色：nft_attribute_clothes_red_{nft_attribute_meta_id}
+     * 性别/雄性：nft_attribute_sex_male_{nft_attribute_meta_id}
+     * 性别/雌性：nft_attribute_sex_female_{nft_attribute_meta_id}
      */
     String key;
 
     /**
      * nft_attribute_meta::value
+     * 不需要国际化
      */
     String value;
 
