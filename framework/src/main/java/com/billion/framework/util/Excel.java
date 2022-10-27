e66cb4844b9f90947b25751fe8e5b1050d1eaf88
任健 <renjian@192.168.0.100>
2022-10-27 18:15:21 +0800

    readmerage

--- framework/src/main/java/com/billion/framework/util/Excel.java
+++ framework/src/main/java/com/billion/framework/util/Excel.java
@@ -5,6 +5,7 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
@@ -58,6 +59,48 @@ public class Excel {
         return resultList;
     }
 
+
+    public static <T> List<T> readMergeColumnExcel(String path, Class<T> clazz, String sheetName) throws IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
+        FileInputStream inputStream = new FileInputStream(new File(path));
+        Workbook workbook = new XSSFWorkbook(inputStream);
+        Sheet sheet = workbook.getSheet(sheetName);
+        Map<String, Integer> results = new HashMap<>();
+        List<T> resultList = new ArrayList<>();
+        Row headRow = sheet.getRow(0);
+        Map<Integer, String> headMap = new HashMap<>();
+        for (int i = 0;;i++) {
+            Cell cell = headRow.getCell(i);
+            if (Objects.isNull(cell)) {
+                break;
+            }
+            headMap.put(i, headRow.getCell(i).getStringCellValue());
+        }
+
+        for (int rowNum = 1; ; rowNum++) {
+            Row row = sheet.getRow(rowNum);
+            if (Objects.isNull(row)) {
+                break;
+            }
+            T t = clazz.getDeclaredConstructor().newInstance();
+            for (int cellNum = 0; cellNum < headMap.keySet().size(); cellNum ++) {
+                row.getCell(cellNum).setCellType(Cell.CELL_TYPE_STRING);
+                String cell = row.getCell(cellNum).getStringCellValue();
+                if (isMergedColumn(sheet, rowNum, cellNum)) {
+                    cell = getMergedColumnValue(sheet, rowNum, cellNum);
+                }
+                Method method = getClassMethod(clazz, headMap.get(cellNum));
+                if (method == null) {
+                    continue;
+                }
+                method.invoke(t, cell);
+            }
+            resultList.add(t);
+        }
+
+        return resultList;
+    }
+
+
     public static <T> boolean writeExcel(String path, List<T> dataList, String sheetName) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, IOException {
         Workbook workbook = new XSSFWorkbook();
         Sheet sheet = workbook.createSheet(sheetName);
@@ -81,8 +124,54 @@ public class Excel {
         return true;
     }
 
-    private static Method getClassGetMethod(Class clazz, String fieldName) throws NoSuchMethodException {
-        return clazz.getMethod("get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1));
+
+    /**
+     * 判断合并了行
+     * @param sheet
+     * @param row
+     * @param column
+     * @return
+     */
+    private static boolean isMergedColumn(Sheet sheet, int row ,int column) {
+        int sheetMergeCount = sheet.getNumMergedRegions();
+        for (int i = 0; i < sheetMergeCount; i++) {
+            CellRangeAddress range = sheet.getMergedRegion(i);
+            int firstColumn = range.getFirstColumn();
+            int lastColumn = range.getLastColumn();
+            int firstRow = range.getFirstRow();
+            int lastRow = range.getLastRow();
+            if (column == firstColumn && column == lastColumn) {
+                if (row >= firstRow && row <= lastRow) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static String getMergedColumnValue(Sheet sheet ,int row , int column) {
+        int sheetMergeCount = sheet.getNumMergedRegions();
+        for (int i = 0; i < sheetMergeCount; i++) {
+            CellRangeAddress ca = sheet.getMergedRegion(i);
+            int firstColumn = ca.getFirstColumn();
+            int lastColumn = ca.getLastColumn();
+            int firstRow = ca.getFirstRow();
+            int lastRow = ca.getLastRow();
+            if (column >= firstColumn && column <= lastColumn) {
+                if (row >= firstRow && row <= lastRow) {
+                    Row fRow = sheet.getRow(firstRow);
+                    Cell fCell = fRow.getCell(firstColumn);
+                    fCell.setCellType(Cell.CELL_TYPE_STRING);
+                    return fCell.getStringCellValue();
+                }
+            }
+        }
+        return null;
+    }
+
+
+    private static Method getClassGetMethod(Class clazz, String fieldName) throws NoSuchMethodException{
+        return  clazz.getMethod("get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1));
     }
 
     private static Method getClassMethod(Class aClazz, String methodName) {
