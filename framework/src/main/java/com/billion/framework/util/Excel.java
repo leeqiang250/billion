8cb271718d672d2155bb815075efe3c91c25c86f
任健 <renjian@192.168.0.100>
2022-10-27 19:52:32 +0800

    writemerge

--- framework/src/main/java/com/billion/framework/util/Excel.java
+++ framework/src/main/java/com/billion/framework/util/Excel.java
@@ -1,12 +1,11 @@
 package com.billion.framework.util;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFColor;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.checkerframework.checker.units.qual.A;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -102,12 +101,27 @@ public class Excel {
 
 
     public static <T> boolean writeExcel(String path, List<T> dataList, String sheetName) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, IOException {
+        if (dataList.size() < 1) {
+            return true;
+        }
         Workbook workbook = new XSSFWorkbook();
         Sheet sheet = workbook.createSheet(sheetName);
-        for (int i = 0; i < dataList.size(); i++) {
-            T t = dataList.get(i);
+
+        Field[] fields = dataList.get(0).getClass().getDeclaredFields();
+        // 生成表头
+        Row headRow = sheet.createRow(0);
+        for (int fieldNum = 0; fieldNum < fields.length; fieldNum++) {
+            CellStyle headStyle = workbook.createCellStyle();
+            Font font = workbook.createFont();
+            font.setBoldweight(Font.BOLDWEIGHT_BOLD);
+            headStyle.setFont(font);
+            Cell cell = headRow.createCell(fieldNum);
+            cell.setCellValue(fields[fieldNum].getName());
+            cell.setCellStyle(headStyle);
+        }
+        for (int i = 1; i <= dataList.size(); i++) {
+            T t = dataList.get(i - 1);
             Class clazz = t.getClass();
-            Field[] fields = clazz.getDeclaredFields();
             Row row = sheet.createRow(i);
             for (int fieldNum = 0; fieldNum < fields.length; fieldNum++) {
                 Cell cell = row.createCell(fieldNum);
@@ -117,6 +131,95 @@ public class Excel {
                 cell.setCellValue(value);
             }
         }
+        FileOutputStream fileOutputStream = new FileOutputStream(path);
+        workbook.write(fileOutputStream);
+        //关闭流
+        fileOutputStream.close();
+        return true;
+    }
+
+
+    public static <T> boolean writeMergeColumnExcel(String path, List<T> dataList, String sheetName, String mergeColumnName) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, IOException {
+        if (dataList.size() < 1) {
+            return true;
+        }
+        Map<String, List<T>> dataMap = new HashMap<>(dataList.size());
+        Field[] fields = dataList.get(0).getClass().getDeclaredFields();
+        //记录要合并列在字段数组中的位置
+        Integer mergeFieldSite = 0;
+        //根据合并字段组装数据
+        for (int i = 0; i < dataList.size(); i++) {
+            T t = dataList.get(i);
+            for (int fieldNum = 0; fieldNum < fields.length; fieldNum++) {
+                Field field = fields[fieldNum];
+                Method method = getClassGetMethod(t.getClass(), field.getName());
+                String value = String.valueOf(method.invoke(t));
+                if (field.getName().equalsIgnoreCase(mergeColumnName)) {
+                    mergeFieldSite = fieldNum;
+                    List<T> valueList = new ArrayList<>();
+                    if (dataMap.containsKey(value)) {
+                        valueList = dataMap.get(value);
+                    }
+                    valueList.add(t);
+                    dataMap.put(value, valueList);
+                    break;
+                }
+            }
+        }
+
+
+        if (mergeFieldSite != 0) {
+            //如果要合并的字段不是在第一个，则将其移动到第一个
+            Field sitField = fields[mergeFieldSite];
+            fields[mergeFieldSite] = fields[0];
+            fields[0] = sitField;
+        }
+
+
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet(sheetName);
+
+        // 生成表头
+        Row headRow = sheet.createRow(0);
+        for (int fieldNum = 0; fieldNum < fields.length; fieldNum++) {
+            CellStyle headStyle = workbook.createCellStyle();
+            Font font = workbook.createFont();
+            font.setBoldweight(Font.BOLDWEIGHT_BOLD);
+            headStyle.setFont(font);
+            Cell cell = headRow.createCell(fieldNum);
+            cell.setCellValue(fields[fieldNum].getName());
+            cell.setCellStyle(headStyle);
+
+        }
+
+        Integer rowNum = 1;
+        List<List<Integer>> mergeParams = new ArrayList<>();
+        for (List<T> itemList : dataMap.values()) {
+
+            List<Integer> mergeParam = new ArrayList<>(4);
+            mergeParam.add(rowNum);
+            mergeParam.add(rowNum + itemList.size() -1);
+            mergeParam.add(0);
+            mergeParam.add(0);
+            mergeParams.add(mergeParam);
+
+            for (T data : itemList) {
+                Row row = sheet.createRow(rowNum);
+                rowNum++;
+                for (int fieldNum = 0; fieldNum < fields.length; fieldNum++) {
+                    Cell cell = row.createCell(fieldNum);
+                    Field field = fields[fieldNum];
+                    Method method = getClassGetMethod(data.getClass(), field.getName());
+                    String value = String.valueOf(method.invoke(data));
+                    cell.setCellValue(value);
+                }
+            }
+        }
+
+        for (List<Integer> list : mergeParams) {
+            sheet.addMergedRegion(new CellRangeAddress(list.get(0), list.get(1), list.get(2), list.get(3)));
+        }
+
         FileOutputStream fileOutputStream = new FileOutputStream(path);
         workbook.write(fileOutputStream);
         //关闭流
